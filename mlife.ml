d22f71d5e7eb3e4dc0a160aabb0aba8cf20b2e51
Jeremy <saturn5971@gmail.com>
2016-09-17 15:00:27 -0400

    Added a number of command line arguments
    Can specify board width/height, cell density, and a filename where the
    initial state of the board can be given. "Ticks" have been made a named
    argument.
    
    If no file is given, the initial board state is now
    randomized.

--- mlife.ml
+++ mlife.ml
@@ -1,9 +1,14 @@
 open Core.Std
 
-let dimx = 10
-let dimy = 10
-
 type cell = Alive | Dead
+
+let get_dims board =
+    (Array.length board, Array.length board.(0))
+
+let wrap max x =
+    (* returns result >= 0 and < max *)
+    let result = x mod max in
+    if result < 0 then max + result else result
 
 let show_cell cell =
     let str = match cell with
@@ -12,17 +17,16 @@ let show_cell cell =
     in printf "%s" str
 
 let show_board board =
-    Array.iter board ~f:(fun row ->
-        Array.iter ~f:show_cell row;
+    let (dimx, dimy) = get_dims board in
+    for y = 0 to dimy - 1 do
+        for x = 0 to dimx - 1 do
+            show_cell board.(x).(y)
+        done;
         printf "\n"
-    )
-
-let wrap max x =
-    (* returns result >= 0 and < max *)
-    let result = x mod max in
-    if result < 0 then max + result else result
+    done
 
 let count_alive board (x, y) =
+    let (dimx, dimy) = get_dims board in
     let my_wrap (x, y) = (wrap dimx x, wrap dimy y) in
     let adjacent_coords = List.map ~f:my_wrap [
         (x - 1, y - 1); (x, y - 1); (x + 1, y - 1);
@@ -41,6 +45,7 @@ let new_state board (x, y) =
         | Dead -> if count = 3 then Alive else Dead
 
 let tick board =
+    let (dimx, dimy) = get_dims board in
     let new_board = Array.make_matrix ~dimx ~dimy Dead in
     for i = 0 to Array.length board - 1 do
         for j = 0 to Array.length board.(i) - 1 do
@@ -49,36 +54,81 @@ let tick board =
     done;
     new_board
 
-let rec loop board count =
-    show_board board;
-    printf "\n";
-    match count with
-        | 1 -> ()
-        | _ -> loop (tick board) (count - 1)
-
-
 let read_file filename =
     let int_tuple = fun (x, y) ->
         (Int.of_string x, Int.of_string y) in
     In_channel.with_file filename ~f:(fun file ->
         In_channel.fold_lines file ~init:[] ~f:(fun coords line ->
-            (String.lsplit2_exn line ~on:',' |> int_tuple) :: coords))
+            if line.[0] = '*' then coords else
+                match String.lsplit2 line ~on:',' with
+                    | None -> printf "Warning: Error parsing board description. \
+                    Skipping line: %s\n" line; coords
+                    | Some t -> (int_tuple t) :: coords))
 
-let main ticks =
+let rec loop board ticks_left =
+    show_board board;
+    printf "\n";
+    match ticks_left with
+        | 1 -> ()
+        | _ -> loop (tick board) (ticks_left - 1)
+
+let make_list count ~f =
+    let rec r count ~f acc =
+        match count with
+            | 0 -> acc
+            | _ -> r (count - 1) ~f ((f ()) :: acc)
+        in
+    r count ~f []
+
+let main ticks width height density filename () =
+    Random.self_init ();
+
+    let ticks = match ticks with
+        | None -> 3
+        | Some t -> t
+    in
+
+    let dimx = match width with
+        | None -> 10
+        | Some w -> w
+    in
+
+    let dimy = match height with
+        | None -> 10
+        | Some h -> h
+    in
+
+    let num_generated =
+        let cells = dimx * dimy in
+        match density with
+            | None -> cells
+            | Some d -> (float cells) *. d /. 100.0 |> Float.iround_exn
+    in
+
+    let live_cells = match filename with
+        | None -> make_list num_generated ~f:(fun () -> (Random.int dimx, Random.int dimy))
+        | Some f -> read_file f
+    in
+
     let board = Array.make_matrix ~dimx ~dimy Dead in
-    List.iter (read_file "Life") ~f:(fun (x, y) -> board.(x).(y) <- Alive);
+    List.iter live_cells  ~f:(fun (x, y) -> board.(x).(y) <- Alive);
     loop board ticks
 
 let spec =
     let open Command.Spec in
     empty
-    +> anon ("ticks" %: int)
+    +> flag "-t" (optional int) ~doc:"Number of ticks to show. 0 to never end."
+    +> flag "-w" (optional int) ~doc:"Width of the board."
+    +> flag "-h" (optional int) ~doc:"Height of the board."
+    +> flag "-d" (optional float) ~doc:"Density of live cells in randomly \
+        generated initial state."
+    +> flag "-f" (optional file)  ~doc:"File specifying the board's initial \
+        state. If no file is given, the initial state is generated randomly."
 
 let command =
     Command.basic
-        ~summary:"Run Conway's Game of Life"
-        ~readme:(fun () -> "Enter the number of ticks you want to show. 0 to never end.")
+        ~summary:"A program simulating John Conway's \"Game of Life\"."
         spec
-        (fun ticks () -> main ticks)
+        main
 
 let () = Command.run command
